86947bcec06f168f46b8910c8ad5828cf4acd590
howardreith <howard.reith@gmail.com>
2018-10-31 10:34:36 -0400

    Add functioning movie analysis
    -Add functioning code for finding similar movies.

---
+++ movies.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+
+rating_columns = ['user_id', 'movie_id', 'rating']
+ratings = pd.read_csv('u.data', sep='\t', names=rating_columns, usecols=range(3), encoding='ISO-8859-1')
+
+movie_columns = ['movie_id', 'title']
+movies = pd.read_csv('u.item', sep='|', names=movie_columns, usecols=range(2), encoding="ISO-8859-1")
+
+ratings = pd.merge(movies, ratings)
+
+movieRatings = ratings.pivot_table(index=['user_id'],columns=['title'],values='rating')
+
+starWarsRatings = movieRatings['Star Wars (1977)']
+
+similarMovies = movieRatings.corrwith(starWarsRatings)
+similarMovies = similarMovies.dropna()
+similarMovies.sort_values(ascending=False)
+
+movieStats = ratings.groupby('title').agg({'rating': [np.size, np.mean]})
+
+popularMovies = movieStats['rating']['size'] >= 100
+movieStats[popularMovies].sort_values([('rating', 'mean')], ascending=False)[:15]
+
+dataframe = movieStats[popularMovies].join(pd.DataFrame(similarMovies, columns=['similarity']))
+print dataframe.sort_values(['similarity'], ascending=False)[:15]
